c564917f8e07353bb198c423a03339887eaa3a5d
Alan Peters <alan3051@gmail.com>
2017-10-01 14:48:01 -0600

    Adding Validation

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,4 +1,13 @@
 class Professor < ApplicationRecord
   has_many :sections
   has_many :courses, through: :sections
+
+  validates :name, :department,  presence: true
+  validates :name, length: { minimum: 3 }
+  validates :department, length: { minimum: 3 }
+
+  validates :department, format: { with: /\A[a-z A-Z]+\z/ }
+
+  #only one professor with the same name in the department
+  validates :name, uniqueness: { scope: :department }
 end
