83b0353687fc5af2557fa369c76ef9456e3551e9
Alan Peters <alan3051@gmail.com>
2017-10-08 15:52:02 -0600

    Adding basic relations for students to professors and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,9 @@ class Course < ApplicationRecord
   has_many :sections
   has_many :professors, through: :sections
 
+  has_many :enrollments
+  has_many :students, through: :enrollments
+
   validates :name, :description, :department, presence: true
   validates :name, length: { minumum: 4 }
   validates :department, length: {minimum: 3} 
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ApplicationRecord
+  has_many :enrollments
+  has_many :sections, through: :enrollments
+  has_many :courses, through: :sections
+  has_many :professors, through: :sections
 end
+
